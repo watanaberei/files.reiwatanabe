10b04914c90ab993e98d9fb2e2eaa696c1009a18
watanaberei <37537870+watanaberei@users.noreply.github.com>
2025-08-15 14:23:06 -0700

    Create figmaData.js

---
+++ data/figmaData.js
@@ -0,0 +1,20 @@
+  export const modes = {
+    design: {
+      footer:       { property: "footer",             state: "false" },
+      viewport:     { property: "viewport-controls",  state: "true"  },
+      pageSelector: { property: "page-selector",      state: "false" },
+      theme:        { property: "theme",              state: "system"}
+    },
+    prototype: {
+      footer:       { property: "footer",             state: "true"  },
+      viewport:     { property: "viewport-controls",  state: "false" },
+      pageSelector: { property: "page-selector",      state: "true"  },
+      theme:        { property: "theme",              state: "dark"  }
+    },
+    dev: {
+      footer:       { property: "footer",             state: "false" },
+      viewport:     { property: "viewport-controls",  state: "false" },
+      pageSelector: { property: "page-selector",      state: "false" },
+      theme:        { property: "theme",              state: "light" }
+    }
+  };
